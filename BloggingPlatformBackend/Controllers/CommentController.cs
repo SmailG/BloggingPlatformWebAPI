49c63a04477ed9d91b2a432c7c45a5520545fc9e
Smail Galijasevic <sgalijasevic@outlook.com>
2018-07-10 13:36:01 +0200

    Added controllers

---
+++ BloggingPlatformBackend/Controllers/CommentController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BloggingPlatformBackend.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BloggingPlatformBackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CommentController : ControllerBase
+    {
+        private readonly BloggingPlatformDB db;
+
+        public CommentController(BloggingPlatformDB _db)
+        {
+            db = _db;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Tag>> Get()
+        {
+            return db.Tags.ToList();
+        }
+    }
+}
---
+++ BloggingPlatformBackend/Controllers/TagController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BloggingPlatformBackend.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BloggingPlatformBackend.Controllers
+{
+    [Route("api/tags")]
+    [ApiController]
+    public class TagController : ControllerBase
+    {
+        private readonly BloggingPlatformDB db;
+
+        public TagController(BloggingPlatformDB _db)
+        {
+            db = _db;
+        }
+    }
+}
